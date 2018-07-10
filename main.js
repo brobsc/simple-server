598fffb9c71ed4463408df5ecf27507b1c722d36
Bruno <bruno.robsiq@gmail.com>
2018-07-10 20:53:23 -0300

    Handle subdomain requests

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 #!/usr/bin/env node
 
 const express = require('express')
+const vhost = require('vhost')
 const path = require('path')
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'static')))
+app.locals.hostname = 'localhost'
+
+// app.use(vhost('*localhost', app));
 
 app.get('/', (req, res) => {
   console.log(`${req.connection.remoteAddress} has connected`)
+  if (app.locals.hostname != req.hostname) {
+    console.log(`req has tried '${req.subdomains.join('.')}' subdomain. sending base`)
+    res.redirect(app.locals.hostname + ':80')
+    return
+  }
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.listen(8080, () => console.log('Listening on port 8080:'))
+app.listen(8080, () => console.log('Listening on port 8080'))
