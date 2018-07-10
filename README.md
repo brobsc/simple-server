df4de8e3978ddc2d3a8de27e48a03e525d1b477e
Bruno <bruno.robsiq@gmail.com>
2018-07-10 11:39:24 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# blackrole
+
+Simple server
---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'static')))
+
+app.get('/', (req, res) => {
+  console.log(`${req.connection.remoteAddress} has connected`)
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(8080, () => console.log('Listening on port 8080:'))
