9d12373dd69aff86de4ff51cd894afd39a3d68a4
Bruno <bruno.robsiq@gmail.com>
2018-07-10 20:55:17 -0300

    Fix wrong hostname

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const path = require('path')
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'static')))
-app.locals.hostname = 'localhost'
+app.locals.hostname = 'brobsc.me'
 
 // app.use(vhost('*localhost', app));
 
