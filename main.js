fe683988d9de927b76fe3dc3c790a25209be21b2
Bruno <bruno.robsiq@gmail.com>
2018-07-10 21:10:01 -0300

    Fix redirection

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
   console.log(`${req.connection.remoteAddress} has connected`)
   if (app.locals.hostname != req.hostname) {
     console.log(`req has tried '${req.subdomains.join('.')}' subdomain. sending base`)
-    res.redirect(app.locals.hostname + ':80')
+    res.redirect(`http://${app.locals.hostname}:80`)
     return
   }
   res.sendFile(path.join(__dirname, 'index.html'))
