0db70590dd6caa538fab7b1c9532629e974c4dda
Bruno <bruno.robsiq@gmail.com>
2018-07-11 15:50:30 -0300

    Add blog

--- main.js
+++ main.js
@@ -4,18 +4,22 @@ const express = require('express')
 const vhost = require('vhost')
 const path = require('path')
 const app = express()
+const blackrole = require('../blackrole')
 
 app.use(express.static(path.join(__dirname, 'static')))
 app.locals.hostname = 'brobsc.me'
 
-// app.use(vhost('*localhost', app));
+app.use(vhost('blog.' + app.locals.hostname, (req, res, next) => {
+  console.log('Blog reached')
+  blackrole.app.handle(req, res, next)
+}))
+app.use(vhost('*' + app.locals.hostname, app));
 
 app.get('/', (req, res) => {
   console.log(`${req.connection.remoteAddress} has requested`)
-  if (app.locals.hostname != req.hostname) {
+  if (app.locals.hostname !== req.hostname) {
     console.log(`req has tried '${req.subdomains.join('.')}' subdomain. sending base`)
     res.redirect(`http://${app.locals.hostname}:80`)
-    return
   }
   res.sendFile(path.join(__dirname, 'index.html'))
 })
