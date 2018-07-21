c9f525488b6b4170f2faaaaa060dcb1c73aa3840
Bruno <bruno.robsiq@gmail.com>
2018-07-21 16:38:24 -0300

    Try again to fix invalid header setting

--- main.js
+++ main.js
@@ -15,14 +15,16 @@ app.use(vhost('blog.' + app.locals.hostname, (req, res, next) => {
 }))
 app.use(vhost('*' + app.locals.hostname, app));
 
-app.get('/', (req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log(`${req.connection.remoteAddress} has requested`)
   if (app.locals.hostname !== req.hostname) {
     console.log(`req has tried '${req.subdomains.join('.')}' subdomain. sending base`)
     res.redirect(`http://${app.locals.hostname}:80`)
   }
   next()
-}, (req, res) => {
+})
+
+app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
