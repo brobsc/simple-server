5804ec32e4f166dbe82835a9edd2163d8eee8ea2
Bruno <bruno.robsiq@gmail.com>
2018-07-21 17:29:56 -0300

    Fix header setting after response sent

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ const app = express()
 const blackrole = require('../blackrole')
 
 app.use(express.static(path.join(__dirname, 'static')))
-app.locals.hostname = 'brobsc.me'
+app.locals.hostname = 'brobsc.test'
+
+if (process.env.NODE_ENV === 'production') {
+  app.locals.hostname = 'brobsc.me'
+} else {
+  console.log('Running on dev mode')
+}
 
 app.use(vhost('blog.' + app.locals.hostname, (req, res, next) => {
   console.log('Blog reached')
@@ -15,16 +21,25 @@ app.use(vhost('blog.' + app.locals.hostname, (req, res, next) => {
 }))
 app.use(vhost('*' + app.locals.hostname, app));
 
-app.use('/', (req, res, next) => {
+redirectToBase = (req, res, next) => {
   console.log(`${req.connection.remoteAddress} has requested`)
   if (app.locals.hostname !== req.hostname) {
     console.log(`req has tried '${req.subdomains.join('.')}' subdomain. sending base`)
+    // res.writeHead(302, {
+    //   Location: `http://${app.locals.hostname}:8080`
+    // })
     res.redirect(`http://${app.locals.hostname}:80`)
+    console.log('header changed')
+  } else {
+    console.log('sending next')
+    next()
   }
-  next()
-})
+}
+
+app.use('/', redirectToBase)
 
 app.get('/', (req, res, next) => {
+  console.log('Sending index.html')
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
